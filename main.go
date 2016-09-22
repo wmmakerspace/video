8147e87b0b3a724a8b096beae0d8e443be4709ae
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-09-22 15:42:09 -0400

    first

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "github.com/wmmakerspace/data-stream-server"
+)
+
+var port = ":80"
+
+func main() {
+    streamserver.Start("/video")
+
+    log.Println("Server listening on port " + port)
+    log.Println("--------------------------------")
+    log.Fatal(http.ListenAndServe(port, nil))
+}
